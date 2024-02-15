cbad81418abd70d45a0109016e5f64982bcb0698
KalashKundaliyacyber <kalashkundaliya@gmail.com>
2024-02-15 22:33:51 +0530

    rotate number

--- Rotate a number/rotate_number.java
+++ Rotate a number/rotate_number.java
@@ -12,12 +12,23 @@ public class rotate_number {
     }
      static void rotate_number(int num, int rc)
     {
-        int value = rc%4;
+        int value = rc%4 ;
    
         int x = num%(int)Math.pow(10, value); // last value 
         int y = num/(int)Math.pow(10, value); // first value 
 
         int z = x*(int)Math.pow(10,4- value) + y; // total value 
         System.out.println(z);
+
+// using for loop 
+        // int i =0;
+        //  for (i=0;i<=rc;i++)
+        // {
+        //     System.out.println((i)+" value : "+num);
+        //     int x = num%10;
+        //     int y = num /10;
+        //     int z = x*1000+y;
+        //     num  = z ;
+        // }
     }
 }
