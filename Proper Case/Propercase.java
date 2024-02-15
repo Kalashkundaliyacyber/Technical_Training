4877a61301a50fbfec1435c40645d3350ebb8c78
KalashKundaliyacyber <kalashkundaliya@gmail.com>
2024-02-16 00:02:04 +0530

    add proper case

---
+++ Proper Case/Propercase.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Propercase {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter the Name : ");
+        String name = sc.nextLine();
+        ProperCase(name);
+    }
+    static void ProperCase(String name) {
+        String properName = "";
+        String[] nameArray = name.split(" ");
+        for (int i = 0; i < nameArray.length; i++) {
+            String firstChar = String.valueOf(nameArray[i].charAt(0)).toUpperCase();
+            String remainingName = nameArray[i].substring(1).toLowerCase();
+
+            properName = properName + " " + firstChar + remainingName;
+        }
+       System.out.println(properName);
+    }
+}
