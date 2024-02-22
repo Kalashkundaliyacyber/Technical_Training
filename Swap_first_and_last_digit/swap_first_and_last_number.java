4447d95037b0104f4cb93bb887e5be4f717507d3
KalashKundaliyacyber <kalashkundaliya@gmail.com>
2024-02-22 19:08:58 +0530

    add new

---
+++ Swap_first_and_last_digit/swap_first_and_last_number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class swap_first_and_last_number
+{
+    public  static void main (String [] args)
+    {   
+         Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+        int n = num;
+        int ln = num %10;
+        int len = String.valueOf(num).length();
+        int fn = num/(int )Math.pow(10,(len-1));
+        int center = (n%(int )Math.pow(10,(len-1)))/10;
+        int value = (((ln * (int)Math.pow(10, len-2)) + center)*10)+fn;
+        System.out.println(value);
+
+    }
+}
